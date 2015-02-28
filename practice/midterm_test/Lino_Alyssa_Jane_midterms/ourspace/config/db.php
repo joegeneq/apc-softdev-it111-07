f80aa2a72b7768ade847cf3661bffb506d25b9c9
Alyssa Jane Lino <alihjane.lino@gmail.com>
2015-02-28 10:33:28 +0800

    STEP E: Change the database connection of ourspace

--- practice/midterm_test/Lino_Alyssa_Jane_midterms/ourspace/config/db.php
+++ practice/midterm_test/Lino_Alyssa_Jane_midterms/ourspace/config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=localhost;dbname=softdev_lino_alyssa_jane_ourspace',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',

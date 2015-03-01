ceff61063a8e6c5df8ec0ce629c41d30556aab2f
Deborah Binag <deborah.binag@gmail.com>
2015-03-01 12:53:00 +0800

    Modify my database Binag_Deborah_midterms/softdev_binag_deborah_ourspace.sql

--- practice/midterm_test/Binag_Deborah_midterms/softdev_binag_deborah_ourspace.sql
+++ practice/midterm_test/Binag_Deborah_midterms/softdev_binag_deborah_ourspace.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 28, 2015 at 04:17 AM
+-- Generation Time: Mar 01, 2015 at 05:51 AM
 -- Server version: 5.6.14
 -- PHP Version: 5.5.6
 
@@ -30,11 +30,12 @@ CREATE TABLE IF NOT EXISTS `myaddress` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `firstname` varchar(30) NOT NULL,
   `middlename` varchar(30) NOT NULL,
+  `lastname` varchar(30) NOT NULL,
   `gender` varchar(1) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `home_address` varchar(50) DEFAULT NULL,
-  `landline` varchar(20) NOT NULL,
-  `cellphone` varchar(20) NOT NULL,
+  `landline` varchar(20) DEFAULT NULL,
+  `cellphone` varchar(20) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 

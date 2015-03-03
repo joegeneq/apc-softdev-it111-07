5e23062ba9aa1036773108e5daed9a9cb20cb75d
Deborah Binag <deborah.binag@gmail.com>
2015-03-03 17:23:25 +0800

    Generated CRUD for Events model

---
+++ application/ampaphil/views/events/_form.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\Events */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="events-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'TALENT_id')->textInput() ?>
+
+    <?= $form->field($model, 'TALENT_MANAGER_id')->textInput() ?>
+
+    <?= $form->field($model, 'EVENTS_DETAILS_id')->textInput() ?>
+
+    <?= $form->field($model, 'CLIENT_id')->textInput() ?>
+
+    <div class="form-group">
+        <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>

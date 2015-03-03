26457c008668682f5bee86b74836782223f2f09d
Deborah Binag <deborah.binag@gmail.com>
2015-03-03 17:18:05 +0800

    Generated Client CRUD controllers and view.php

---
+++ application/ampaphil/views/client/view.php
@@ -0,0 +1,44 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\DetailView;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\Client */
+
+$this->title = $model->id;
+$this->params['breadcrumbs'][] = ['label' => 'Clients', 'url' => ['index']];
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="client-view">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        <?= Html::a('Update', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
+        <?= Html::a('Delete', ['delete', 'id' => $model->id], [
+            'class' => 'btn btn-danger',
+            'data' => [
+                'confirm' => 'Are you sure you want to delete this item?',
+                'method' => 'post',
+            ],
+        ]) ?>
+    </p>
+
+    <?= DetailView::widget([
+        'model' => $model,
+        'attributes' => [
+            'id',
+            'CLIENT_LName',
+            'CLIENT_FName',
+            'CLIENT_MName',
+            'CLIENT_Company',
+            'CLIENT_CompanyBlkOrAreaNo',
+            'CLIENT_CompanyBrgy',
+            'CLIENT_ContactNo',
+            'CLIENT_CompanyCity',
+            'CLIENT_EmailAdd:email',
+        ],
+    ]) ?>
+
+</div>

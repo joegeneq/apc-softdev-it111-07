effc183908b54699373b604ee62f0446b4af4873
Alyssa Jane Lino <alihjane.lino@gmail.com>
2015-03-06 21:50:27 +0800

    UPDATED site.css, index.php, main.php and SiteController.php

--- application/ampaphil_advanced/backend/views/layouts/main.php
+++ application/ampaphil_advanced/backend/views/layouts/main.php
@@ -25,7 +25,7 @@ AppAsset::register($this);
     <div class="wrap">
         <?php
             NavBar::begin([
-                'brandLabel' => 'My Company',
+                'brandLabel' => 'Artist Manager Placement Agency Philippines Inc.',
                 'brandUrl' => Yii::$app->homeUrl,
                 'options' => [
                     'class' => 'navbar-inverse navbar-fixed-top',

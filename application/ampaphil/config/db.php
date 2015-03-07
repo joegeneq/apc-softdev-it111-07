13c89dc15adea08a726be27276f715d67b422084
Danica Faith Parian <danica.parian@gmail.com>
2015-03-07 11:37:30 +0800

    Changed the database connection

--- application/ampaphil/config/db.php
+++ application/ampaphil/config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=ampaphil',
+    'dsn' => 'mysql:host=localhost;dbname=ampaphil_bms',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',

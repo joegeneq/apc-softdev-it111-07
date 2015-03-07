e64684d59c42eb7fbd6d6cf6985f9c9cbc213604
Alyssa Jane Lino <alihjane.lino@gmail.com>
2015-03-08 01:48:37 +0800

    MODIFIED frontend/views/site/about.php

--- application/ampaphil_advanced/frontend/views/site/about.php
+++ application/ampaphil_advanced/frontend/views/site/about.php
@@ -2,13 +2,27 @@
 use yii\helpers\Html;
 
 /* @var $this yii\web\View */
-$this->title = 'About';
-$this->params['breadcrumbs'][] = $this->title;
+$this->title = 'About Artist Managers Placement Agency';
+//$this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-about">
-    <h1><?= Html::encode($this->title) ?></h1>
+	<center><img src="../web/images/ampaphil_logo.jpg" height="" width="800px"></img></center><br>
+    <h3><?= Html::encode($this->title) ?></h3>
+    <p>ASIARABE ENTERPRISES, INC. which was incorporated on May 16, 1987 was shortly changed to Artist Managers Placement Agency of the Philippines and was incorporated on July 2, 1987. AMPAPhil Inc. is a Non–Construction Philippine-based human resource recruitment and deployment agency, which takes pride in providing world-class Filipino professional and skilled workers and performing artist. We are committed to contribute growth and success by providing our client with candidates that match their specification and qualification through our relentless effort to take extra mile in imparting an outstanding service giving us an edge in the overseas employment industry. The estimated number of overseas contract workers deployed as of December 2012 is 16, 105. AMPAPhil Inc. specializes in recruitment, deployment and post deployment services such as:
+	<br>Mobilization, sourcing and pooling of applicants based on clients’ specifications; Preliminary screening and testing prior to final selection by the principal and/or employer; Document processing in POEA and at the Embassy of the country of destination, when necessary; and Orientation, briefing meetings and Pre-Departure Orientation Seminar Artist Managers Placement Agency of the Philippines Inc. is a member of Overseas Placement Association of the Philippines (OPAP) and also a member of Philippines Association Services Exporters INC. (PASEI)</p>
+	<hr>
+	<h3>Application process</h3>
+	<div class="col-lg-4">
+                <h2><center><img src="../web/images/singer-icon.png" height="150px" width="150px"></img></center></h2>
+                <p>Type process here</p>
+            </div>
+            <div class="col-lg-4">
+                <h2><center><img src="../web/images/band-icon.png" height="150px" width="150px"></img></center></h2>
+                <p>Type process here</p>
+            </div>
+            <div class="col-lg-4">
+                <h2><center><img src="../web/images/dancer-icon.png" height="150px" width="150px"></img></center></h2>
+                <p>Type process here</p>
+            </div>
 
-    <p>This is the About page. You may modify the following file to customize its content:</p>
-
-    <code><?= __FILE__ ?></code>
 </div>

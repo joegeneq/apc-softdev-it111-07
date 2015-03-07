db336d0ce7a06a460317684982902eb2d6860cc2
Alyssa Jane Lino <alihjane.lino@gmail.com>
2015-03-07 19:21:12 +0800

    GENERATED model for talent_line table

---
+++ application/ampaphil_advanced/backend/models/TalentLine.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "talent_line".
+ *
+ * @property integer $id
+ * @property string $talent_type
+ * @property string $talent_specialization
+ * @property integer $applicant_id
+ *
+ * @property Applicant $applicant
+ */
+class TalentLine extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'talent_line';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['talent_type', 'talent_specialization'], 'required'],
+            [['applicant_id'], 'integer'],
+            [['talent_type', 'talent_specialization'], 'string', 'max' => 45]
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'talent_type' => 'Talent Type',
+            'talent_specialization' => 'Talent Specialization',
+            'applicant_id' => 'Applicant ID',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getApplicant()
+    {
+        return $this->hasOne(Applicant::className(), ['id' => 'applicant_id']);
+    }
+}

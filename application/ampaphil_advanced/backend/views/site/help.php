de817d58edb5910fcd1061550349364b27304af9
Alyssa Jane Lino <alihjane.lino@gmail.com>
2015-03-08 13:55:19 +0800

    ADDED backend/views/site/help.php

---
+++ application/ampaphil_advanced/backend/views/site/help.php
@@ -0,0 +1,12 @@
+<?php
+/* @var $this yii\web\View */
+$this->title = 'Help';
+
+?>
+<div class="site-index">
+
+    <div class="body-content">
+        <h3>Managing an event</h3>
+        <p>Write steps here to add, edit or delete an event</p>
+    </div>
+</div>

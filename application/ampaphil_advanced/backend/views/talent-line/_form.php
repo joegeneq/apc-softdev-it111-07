f4f5e0726a1dd2fc309a437734c1841ef05c0225
Alyssa Jane Lino <alihjane.lino@gmail.com>
2015-03-08 18:27:41 +0800

    MODIFIED talent-line/_form.php for dropDownList

--- application/ampaphil_advanced/backend/views/talent-line/_form.php
+++ application/ampaphil_advanced/backend/views/talent-line/_form.php
@@ -1,7 +1,9 @@
 <?php
 
 use yii\helpers\Html;
+use yii\helpers\ArrayHelper;
 use yii\widgets\ActiveForm;
+use app\models\Applicant;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\TalentLine */
@@ -16,7 +18,11 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'TALENT_Specialization')->textInput(['maxlength' => 45]) ?>
 
-    <?= $form->field($model, 'APPLICANT_id')->textInput() ?>
+    <?php $form = ActiveForm::begin(); ?>
+    <?= $form->field($model, 'APPLICANT_id')->dropDownList(
+        ArrayHelper::map(Applicant::find()->all(), 'id', 'APP_LName'),
+        ['prompt'=>'Select Lastname'] ) 
+         ?>
 
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>

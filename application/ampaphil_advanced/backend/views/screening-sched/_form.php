32ae99e9312b602e021fea29b3b91996f5e2f8d6
Alyssa Jane Lino <alihjane.lino@gmail.com>
2015-03-08 18:28:15 +0800

    MODIFIED screening-sched/_form.php form dropDownList

--- application/ampaphil_advanced/backend/views/screening-sched/_form.php
+++ application/ampaphil_advanced/backend/views/screening-sched/_form.php
@@ -1,7 +1,9 @@
 <?php
 
 use yii\helpers\Html;
+use yii\helpers\ArrayHelper;
 use yii\widgets\ActiveForm;
+use app\models\Employee;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\ScreeningSched */
@@ -16,9 +18,13 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'SCR_Time')->textInput() ?>
 
-    <?= $form->field($model, 'APP_Status')->textInput(['maxlength' => 10]) ?>
+    <?= $form->field($model, 'APP_Status')->dropDownList(['' => 'Select Status', 'For Screening' => 'For Screening', 'Passed' => 'Passed', 'Failed' => 'Failed']) ?>
 
-    <?= $form->field($model, 'EMPLOYEE_id')->textInput() ?>
+    <?php $form = ActiveForm::begin(); ?>
+    <?= $form->field($model, 'EMPLOYEE_id')->dropDownList(
+        ArrayHelper::map(Employee::find()->all(), 'id', 'EMP_LName'),
+        ['prompt'=>'Select Lastname'] ) 
+    ?>
 
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>

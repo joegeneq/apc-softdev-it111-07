1fa62c609d92616359ea644301083888ba79908e
Alyssa Jane Lino <alihjane.lino@gmail.com>
2015-03-08 18:28:33 +0800

    MODIFIED applicant/index.php

--- application/ampaphil_advanced/backend/views/applicant/index.php
+++ application/ampaphil_advanced/backend/views/applicant/index.php
@@ -17,6 +17,7 @@ $this->params['breadcrumbs'][] = $this->title;
 
     <p>
         <?= Html::a('Create Applicant', ['create'], ['class' => 'btn btn-success']) ?>
+        <?= Html::a('Add Talent_line', ['talent-line/create'], ['class' => 'btn btn-success']) ?>
     </p>
 
     <?= GridView::widget([

a1886880077a3e39496ef2ae31d629c605331fd1
Alyssa Jane Lino <alihjane.lino@gmail.com>
2015-03-08 14:30:59 +0800

    MODIFIED help.php and site.css

--- application/ampaphil_advanced/backend/views/site/help.php
+++ application/ampaphil_advanced/backend/views/site/help.php
@@ -8,5 +8,24 @@ $this->title = 'Help';
     <div class="body-content">
         <h3>Managing an event</h3>
         <p>Write steps here to add, edit or delete an event</p>
+        <hr>
+        <h3>Managing of screening schedule</h3>
+        <p>Write steps here to add, edit or delete an event</p>
+        <hr>
+        <h3>Managing transaction</h3>
+        <p>Write steps here to add, edit or delete an event</p>
+        <hr>
+        <h3>Managing client's record</h3>
+        <p>Write steps here to add, edit or delete an event</p>
+        <hr>
+        <h3>Managing employee's record</h3>
+        <p>Write steps here to add, edit or delete an event</p>
+        <hr>
+        <h3>Managing applicant's record</h3>
+        <p>Write steps here to add, edit or delete an event</p>
+        <hr>
+        <h3>Managing manager's record</h3>
+        <p>Write steps here to add, edit or delete an event</p>
+        <hr>
     </div>
 </div>

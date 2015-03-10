25e379cb87ac57f2f97f89d54c199007909b0644
Alyssa Jane Lino <alihjane.lino@gmail.com>
2015-03-10 16:12:10 +0800

    MODIFIED employee/_form.php for datepicker

--- application/ampaphil_advanced/backend/views/employee/_form.php
+++ application/ampaphil_advanced/backend/views/employee/_form.php
@@ -29,7 +29,7 @@ use dosamigos\datepicker\DatePicker;
             //'template' => '<div class="well well-sm" style="background-color: #fff; width:250px">{input}</div>',
             'clientOptions' => [
                 'autoclose' => true,
-                'format' => 'yyyy-M-dd'
+                'format' => 'yyyy-mm-dd'
             ]
     ]);?>
 

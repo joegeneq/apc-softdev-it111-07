47dc9410ffa0b1b42e0718fa8cd2107071d67ab6
Deborah Binag <deborah.binag@gmail.com>
2015-03-10 19:06:05 +0800

    MODIFIED TalentLineSearch.php for search field

--- application/ampaphil_advanced/backend/models/TalentLineSearch.php
+++ application/ampaphil_advanced/backend/models/TalentLineSearch.php
@@ -18,13 +18,8 @@ class TalentLineSearch extends TalentLine
     public function rules()
     {
         return [
-<<<<<<< HEAD
             [['id'], 'integer'],
-            [['TALENT_Type', 'TALENT_Specialization', 'APPLICANT_id'], 'safe'],
-=======
-            [['id','APPLICANT_id'], 'integer'],
-            [['TALENT_Type','TALENT_Specialization'], 'safe'],
->>>>>>> 52cf3f49c33f37da65b7d58af28d7ec4d4e338e6
+            [['TALENT_Type','TALENT_Specialization','APPLICANT_id'], 'safe'],
         ];
     }
 
@@ -60,23 +55,15 @@ class TalentLineSearch extends TalentLine
             return $dataProvider;
         }
 
-<<<<<<< HEAD
         $query->joinWith('aPPLICANT');
-=======
-
->>>>>>> 52cf3f49c33f37da65b7d58af28d7ec4d4e338e6
         $query->andFilterWhere([
-            'id' => $this->id
+            'id' => $this->id,
+            //APPLICANT_id' => $this->APPLICANT_id,
         ]);
 
-<<<<<<< HEAD
-        $query->andFilterWhere(['like', 'TALENT_Type', $this->TALENT_Type])
-            ->andFilterWhere(['like', 'TALENT_Specialization', $this->TALENT_Specialization])
-            ->andFilterWhere(['like', 'aPPLICANT.APP_LName', $this->APPLICANT_id]);
-=======
         $query->andFilterWhere(['like','TALENT_Type', $this->TALENT_Type])
-        ->andFilterWhere(['like', 'TALENT_Specialization', $this->TALENT_Specialization]);
->>>>>>> 52cf3f49c33f37da65b7d58af28d7ec4d4e338e6
+              ->andFilterWhere(['like', 'TALENT_Specialization', $this->TALENT_Specialization])
+              ->andFilterWhere(['like', 'aPPLICANT.APP_LName', $this->APPLICANT_id]);
 
         return $dataProvider;
     }

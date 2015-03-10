a0eb407440619b312abc5ddc8d28e2006f4bd4bf
Alyssa Jane Lino <alihjane.lino@gmail.com>
2015-03-10 15:56:26 +0800

    MODIFIED talent-line/_form.php

--- application/ampaphil_advanced/backend/views/talent-line/_form.php
+++ application/ampaphil_advanced/backend/views/talent-line/_form.php
@@ -36,7 +36,7 @@ use app\models\Applicant;
     <?= $form->field($model, 'APPLICANT_id')->dropDownList(
         ArrayHelper::map(Applicant::find()->all(), 'id', 'APP_LName'),
         ['prompt'=>'Select Lastname'] ) 
-         ?>
+    ?>
 
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>

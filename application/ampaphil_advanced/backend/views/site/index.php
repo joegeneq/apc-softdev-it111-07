3ef53bea68032a07bb2b14df2709f449ea02c181
Danica Faith Parian <danica.parian@gmail.com>
2015-03-14 11:35:58 +0800

    Modified path of images in backend/views/site/index.php

--- application/ampaphil_advanced/backend/views/site/index.php
+++ application/ampaphil_advanced/backend/views/site/index.php
@@ -22,42 +22,42 @@ $this->title = 'AMPAPhil';
         <div class="row">
 			<div class="col-lg-4">
                 <p><a href="?r=employee">
-                    <img src="../images/icons/employee.png" height="300px" width="280px">
+                    <img src="images/icons/employee.png" height="300px" width="280px">
                 </a></p>
             </div>
 			<div class="col-lg-4">
                 <p><a href="?r=applicant">
-                    <img src="../images/icons/applicants.png" height="300px" width="280px">
+                    <img src="images/icons/applicants.png" height="300px" width="280px">
                 </a></p>
             </div>
 			<div class="col-lg-4">
                 <p><a href="?r=screening-sched">
-                    <img src="../images/icons/screening.png" height="300px" width="280px">
+                    <img src="images/icons/screening.png" height="300px" width="280px">
                 </a></p>
             </div>
 			<div class="col-lg-4">
                 <p><a href="?r=talent">
-                    <img src="../images/icons/talents.png" height="300px" width="280px">
+                    <img src="images/icons/talents.png" height="300px" width="280px">
                 </a></p>
             </div>
 			<div class="col-lg-4">
                 <p><a href="?r=manager">
-                    <img src="../images/icons/manager.png" height="300px" width="280px">
+                    <img src="images/icons/manager.png" height="300px" width="280px">
                 </a></p>
 			</div>
 			<div class="col-lg-4">
                 <p><a href="?r=client">
-					<img src="../images/icons/client.png" height="300px" width="280px">
+					<img src="images/icons/client.png" height="300px" width="280px">
                 </a></p>
             </div>
              <div class="col-lg-4">
                 <p><a href="?r=event-details">
-                    <img src="../images/icons/events.png" height="300px" width="280px">
+                    <img src="images/icons/events.png" height="300px" width="280px">
                 </a></p>
             </div>			
             <div class="col-lg-4">
                 <p><a href="?r=payments">
-                    <img src="../images/icons/transaction.png" height="300px" width="280px">
+                    <img src="images/icons/transaction.png" height="300px" width="280px">
                 </a></p>
             </div>
         </div>

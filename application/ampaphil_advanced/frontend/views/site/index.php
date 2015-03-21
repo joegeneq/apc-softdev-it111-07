1d99ce2fb7cf428a0cd3da0fa377f876e9d3d74d
Alyssa Jane Lino <alihjane.lino@gmail.com>
2015-03-21 18:10:44 +0800

    MODIFIED frontend/views/site/index.php

--- application/ampaphil_advanced/frontend/views/site/index.php
+++ application/ampaphil_advanced/frontend/views/site/index.php
@@ -5,28 +5,28 @@ $this->title = 'AMPAPhil';
 <div class="site-index">
 
     <div class="jumbotron">
-       <img src="images/ampaphil_logo.jpg" height="" width="800px"></img>
+       <img src="../web/images/ampaphil_logo.jpg" height="" width="800px"></img>
     </div>
 
     <div class="body-content">
 
         <div class="row">
             <div class="col-lg-4">
-                <h2><center><img src="images/singer-icon.png" height="70px" width="70px"></img></center></h2>
+                <h2><center><img src="../web/images/singer-icon.png" height="70px" width="70px"></img></center></h2>
 
                 <p><b>Solo singer.</b> A performer who uses his or her voice to entertain on stage, in movies, on television or on recordings. We are accepting applicants for this field. <i>How to apply?</i> Click "more info" to know.</p>
 
                 <p><a class="btn btn-default" href="?r=site%2Fabout">More info &raquo;</a></p>
             </div>
             <div class="col-lg-4">
-                <h2><center><img src="images/band-icon.png" height="70px" width="70px"></img></center></h2>
+                <h2><center><img src="../web/images/band-icon.png" height="70px" width="70px"></img></center></h2>
 
                 <p><b>Band.</b> Group of people working together, particularly in the field of music. We are accepting applicants for this field. <i>How to apply?</i> Click "more info" to know. </p>
 
                 <p><a class="btn btn-default" href="?r=site%2Fabout">More info &raquo;</a></p>
             </div>
             <div class="col-lg-4">
-                <h2><center><img src="images/dancer-icon.png" height="70px" width="70px"></img></center></h2>
+                <h2><center><img src="../web/images/dancer-icon.png" height="70px" width="70px"></img></center></h2>
 
                 <p><b>Dancer. Contemporary, Jazz and Folk dancers.</b> A person who do a series of rhythmic and patterned bodily movements usually performed to music. We are accepting applicants for this field. <i>How to apply?</i> Click "more info" to know.</p>
 

9139782c17af1599e5e3c0c7536fc25a26da3637
Alyssa Jane Lino <alihjane.lino@gmail.com>
2015-03-22 10:22:40 +0800

    MODIFIED event-details/_form.php

--- application/ampaphil_advanced/backend/views/event-details/_form.php
+++ application/ampaphil_advanced/backend/views/event-details/_form.php
@@ -2,9 +2,6 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
-//for widgets
-use dosamigos\datepicker\DatePicker;
-use kartik\time\TimePicker;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\EventDetails */
@@ -15,68 +12,21 @@ use kartik\time\TimePicker;
 
     <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'EVENT_Name')->textInput(['maxlength' => 45]) ?>
+    <?= $form->field($model, 'event_name')->textInput(['maxlength' => 45]) ?>
 
-    <?= $form->field($model, 'EVENT_Location')->textInput(['maxlength' => 45]) ?>
+    <?= $form->field($model, 'event_location')->textInput(['maxlength' => 45]) ?>
 
-    <?= $form->field($model, 'EVENT_Type')->dropDownList(['' => 'Select Event Type',
-                                                          'Party' => 'Party',
-                                                          'Reunion' => 'Reunion',
-                                                          'Debut' => 'Debut',
-                                                          'Wedding' => 'Wedding',
-                                                          'Anniversary' => 'Anniversary',
-                                                          'School Activities' => 'School Activities'
-                                                        ]) ?>
+    <?= $form->field($model, 'event_type')->textInput(['maxlength' => 45]) ?>
 
-    <?= $form->field($model, 'EVENT_DateFrom')->widget(
-        DatePicker::className(), [
-            // inline too, not bad
-            'inline' => false, 
-            // modify template for custom rendering
-            //'template' => '<div class="well well-sm" style="background-color: #fff; width:250px">{input}</div>',
-            'clientOptions' => [
-                'autoclose' => true,
-                'format' => 'yyyy-mm-dd'
-            ]
-    ]);?>
+    <?= $form->field($model, 'event_datefrom')->textInput() ?>
 
-    <?= $form->field($model, 'EVENT_DateTo')->widget(
-        DatePicker::className(), [
-            // inline too, not bad
-            'inline' => false, 
-            // modify template for custom rendering
-            //'template' => '<div class="well well-sm" style="background-color: #fff; width:250px">{input}</div>',
-            'clientOptions' => [
-                'autoclose' => true,
-                'format' => 'yyyy-mm-dd'
-            ]
-    ]);?>
+    <?= $form->field($model, 'event_dateto')->textInput() ?>
 
-    <?= $form->field($model, 'EVENT_TimeFrom')->widget(
-        TimePicker::className(), [
-            'name' => 'time',
-            'pluginOptions' => [
-                'showSeconds' => false,
-                'showMeridian' => false,
-                'minuteStep' => 1,
-                'secondStep' => 5
-            ]
-    ]);?>
+    <?= $form->field($model, 'event_timefrom')->textInput() ?>
 
-    <?= $form->field($model, 'EVENT_TimeTo')->widget(
-        TimePicker::className(), [
-            'name' => 'time',
-            'pluginOptions' => [
-                'showSeconds' => false,
-                'showMeridian' => false,
-                'minuteStep' => 1,
-                'secondStep' => 5
-            ]
-    ]);?>
+    <?= $form->field($model, 'event_timeto')->textInput() ?>
 
-
-
-    <?= $form->field($model, 'EVENT_Status')->dropDownList(['' => 'Select Status', 'Upcoming' => 'Upcoming', 'On Going' => 'On Going', 'Done' => 'Done']) ?>
+    <?= $form->field($model, 'transaction_id')->textInput() ?>
 
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>

2dac6f7314a67a986e2d12b5c33256cde50764b7
Alyssa Jane Lino <alihjane.lino@gmail.com>
2015-03-22 15:18:39 +0800

    AUTO UPDTAED frontend/config/main-local.php

--- application/ampaphil_advanced/frontend/config/main-local.php
+++ application/ampaphil_advanced/frontend/config/main-local.php
@@ -4,7 +4,7 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => 'z4ylchP-J8flo6tNev8Ku1ufXRCw0BKP',
+            'cookieValidationKey' => '_Nyfq20Dxo7M13l3Pp2-q_CFDK6TnSNf',
         ],
     ],
 ];

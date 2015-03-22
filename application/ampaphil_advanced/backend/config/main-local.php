e22e1f8f602e6eddb21a0fdf651da58509ab4740
Alyssa Jane Lino <alihjane.lino@gmail.com>
2015-03-22 13:54:36 +0800

    MODIFIED main-local.php

--- application/ampaphil_advanced/backend/config/main-local.php
+++ application/ampaphil_advanced/backend/config/main-local.php
@@ -4,7 +4,7 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => 'OImR4KQ5li8PAMjcLnATOls1QJFmPkl9',
+            'cookieValidationKey' => 'Mpm8lvnj7xztK838D3IAlAUX1a2ESmHH',
         ],
     ],
 ];

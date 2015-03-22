1ce2109ac583937f6d001545ae59df9059284d20
Alyssa Jane Lino <alihjane.lino@gmail.com>
2015-03-22 14:13:44 +0800

    MODIFIED model for applicant

--- application/ampaphil_advanced/backend/models/Applicant.php
+++ application/ampaphil_advanced/backend/models/Applicant.php
@@ -13,7 +13,7 @@ use Yii;
  * @property string $app_mname
  * @property string $app_gender
  * @property string $app_bdate
- * @property string $app_blockno
+ * @property string $app_blkno
  * @property string $app_street
  * @property string $app_brgy
  * @property string $app_city
@@ -45,11 +45,11 @@ class Applicant extends \yii\db\ActiveRecord
     public function rules()
     {
         return [
-            [['app_lname', 'app_fname', 'app_gender', 'app_bdate', 'app_blockno', 'app_street', 'app_brgy', 'app_city', 'app_contactno', 'app_regdate', 'app_regtime', 'app_talent'], 'required'],
+            [['app_lname', 'app_fname', 'app_gender', 'app_bdate', 'app_blkno', 'app_street', 'app_brgy', 'app_city', 'app_contactno', 'app_regdate', 'app_regtime', 'app_talent'], 'required'],
             [['app_bdate', 'app_regdate', 'app_regtime'], 'safe'],
             [['app_zipcode', 'screening_sched_id'], 'integer'],
             [['app_lname', 'app_fname', 'app_mname', 'app_street', 'app_brgy', 'app_city', 'app_emailadd', 'app_talent'], 'string', 'max' => 45],
-            [['app_gender', 'app_blockno'], 'string', 'max' => 10],
+            [['app_gender', 'app_blkno'], 'string', 'max' => 10],
             [['app_contactno'], 'string', 'max' => 20]
         ];
     }
@@ -61,22 +61,22 @@ class Applicant extends \yii\db\ActiveRecord
     {
         return [
             'id' => 'ID',
-            'app_lname' => 'Last Name',
-            'app_fname' => 'First Name',
-            'app_mname' => 'Middle Name',
-            'app_gender' => 'Gender',
-            'app_bdate' => 'Birth Date',
-            'app_blockno' => 'Block/Lot Number',
-            'app_street' => 'Street',
-            'app_brgy' => 'Barangay',
-            'app_city' => 'City',
-            'app_zipcode' => 'Zip Code',
-            'app_contactno' => 'Contact Number',
-            'app_emailadd' => 'Email Address',
-            'app_regdate' => 'Registration Date',
-            'app_regtime' => 'Registration Time',
-            'app_talent' => 'Talent',
-            'screening_sched_id' => 'Screening Schedule ID',
+            'app_lname' => 'App Lname',
+            'app_fname' => 'App Fname',
+            'app_mname' => 'App Mname',
+            'app_gender' => 'App Gender',
+            'app_bdate' => 'App Bdate',
+            'app_blkno' => 'App Blkno',
+            'app_street' => 'App Street',
+            'app_brgy' => 'App Brgy',
+            'app_city' => 'App City',
+            'app_zipcode' => 'App Zipcode',
+            'app_contactno' => 'App Contactno',
+            'app_emailadd' => 'App Emailadd',
+            'app_regdate' => 'App Regdate',
+            'app_regtime' => 'App Regtime',
+            'app_talent' => 'App Talent',
+            'screening_sched_id' => 'Screening Sched ID',
         ];
     }
 

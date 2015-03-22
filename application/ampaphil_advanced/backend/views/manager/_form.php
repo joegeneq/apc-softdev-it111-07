3db17a33d919d4c27f477119643c6a69f9f5252f
Alyssa Jane Lino <alihjane.lino@gmail.com>
2015-03-22 16:03:51 +0800

    MODIFIED manager/_form.php

--- application/ampaphil_advanced/backend/views/manager/_form.php
+++ application/ampaphil_advanced/backend/views/manager/_form.php
@@ -2,6 +2,7 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use dosamigos\datepicker\DatePicker;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\Manager */
@@ -18,9 +19,22 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'mgr_mname')->textInput(['maxlength' => 45]) ?>
 
-    <?= $form->field($model, 'mge_gender')->textInput(['maxlength' => 10]) ?>
+    <?= $form->field($model, 'mgr_gender')->dropDownList(['' => 'Select Gender',
+                                                          'Male' => 'Male',
+                                                          'Female' => 'Female'
+                                                        ]) ?>
 
-    <?= $form->field($model, 'mgr_bdate')->textInput() ?>
+    <?= $form->field($model, 'mgr_bdate')->widget(
+        DatePicker::className(), [
+            // inline too, not bad
+            'inline' => false, 
+            // modify template for custom rendering
+            //'template' => '<div class="well well-sm" style="background-color: #fff; width:250px">{input}</div>',
+            'clientOptions' => [
+                'autoclose' => true,
+                'format' => 'yyyy-mm-dd'
+            ]
+    ]);?>
 
     <?= $form->field($model, 'mgr_blkno')->textInput(['maxlength' => 10]) ?>
 
@@ -36,7 +50,11 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'mgr_emailadd')->textInput(['maxlength' => 45]) ?>
 
-    <?= $form->field($model, 'mgr_expertise')->textInput(['maxlength' => 20]) ?>
+    <?= $form->field($model, 'mgr_expertise')->dropDownList([ '' => 'Select Expertise',
+                                                            'Dancing' => 'Dancing',
+                                                            'Singing' => 'Singing',
+                                                            'Band' => 'Band'
+                                                            ]) ?>
 
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>

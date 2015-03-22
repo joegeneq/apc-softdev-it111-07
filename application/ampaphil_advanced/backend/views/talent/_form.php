b9e0ffe62744922636eeba9af2bc338cdfc1a4e1
Alyssa Jane Lino <alihjane.lino@gmail.com>
2015-03-22 16:46:47 +0800

    MODIFIED talent/_form.php

--- application/ampaphil_advanced/backend/views/talent/_form.php
+++ application/ampaphil_advanced/backend/views/talent/_form.php
@@ -2,6 +2,13 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use yii\helpers\ArrayHelper;
+use app\models\Applicant;
+use app\models\Manager;
+use app\models\ScreeningSched;
+
+/* for widgets*/
+use dosamigos\datepicker\DatePicker;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\Talent */
@@ -12,15 +19,44 @@ use yii\widgets\ActiveForm;
 
     <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'manager_id')->textInput() ?>
+     <?= $form->field($model, 'manager_id')->dropDownList(
+        ArrayHelper::map(Manager::find()->all(), 'id', 'MGR_LName'),
+        ['prompt'=>'Select Lastname'] ) 
+    ?>
 
-    <?= $form->field($model, 'talent_managedstartdate')->textInput() ?>
+    <?= $form->field($model, 'talent_managedstartdate')->widget(
+        DatePicker::className(), [
+            // inline too, not bad
+            'inline' => false, 
+            // modify template for custom rendering
+            //'template' => '<div class="well well-sm" style="background-color: #fff; width:250px">{input}</div>',
+            'clientOptions' => [
+                'autoclose' => true,
+                'format' => 'yyyy-mm-dd'
+            ]
+    ]);?>
 
-    <?= $form->field($model, 'talent_managedenddate')->textInput() ?>
+    <?= $form->field($model, 'talent_managedenddate')->widget(
+        DatePicker::className(), [
+            // inline too, not bad
+            'inline' => false, 
+            // modify template for custom rendering
+            //'template' => '<div class="well well-sm" style="background-color: #fff; width:250px">{input}</div>',
+            'clientOptions' => [
+                'autoclose' => true,
+                'format' => 'yyyy-mm-dd'
+            ]
+    ]);?>
 
-    <?= $form->field($model, 'screening_sched_id')->textInput() ?>
+     <?= $form->field($model, 'screening_sched_id')->dropDownList(
+        ArrayHelper::map(ScreeningSched::find()->all(), 'id', 'SCR_Time', 'SCR_Date'),
+        ['prompt'=>'Select Schedule'] ) 
+    ?>
 
-    <?= $form->field($model, 'applicant_id')->textInput() ?>
+    <?= $form->field($model, 'applicant_id')->dropDownList(
+        ArrayHelper::map(Applicant::find()->all(), 'id', 'APP_LName'),
+        ['prompt'=>'Select Lastname'] ) 
+    ?>
 
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>

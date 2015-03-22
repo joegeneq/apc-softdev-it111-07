dba0b26412dc6901a55590da1edd7b86c7ff2f11
Alyssa Jane Lino <alihjane.lino@gmail.com>
2015-03-22 15:19:16 +0800

    MODIFIED event-details/_form.php

--- application/ampaphil_advanced/backend/views/event-details/_form.php
+++ application/ampaphil_advanced/backend/views/event-details/_form.php
@@ -2,6 +2,9 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+//for widgets
+use dosamigos\datepicker\DatePicker;
+use kartik\time\TimePicker;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\EventDetails */
@@ -16,15 +19,60 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'event_location')->textInput(['maxlength' => 45]) ?>
 
-    <?= $form->field($model, 'event_type')->textInput(['maxlength' => 45]) ?>
+    <?= $form->field($model, 'event_type')->dropDownList(['' => 'Select Event Type',
+                                                          'Party' => 'Party',
+                                                          'Reunion' => 'Reunion',
+                                                          'Debut' => 'Debut',
+                                                          'Wedding' => 'Wedding',
+                                                          'Anniversary' => 'Anniversary',
+                                                          'School Activities' => 'School Activities'
+                                                        ]) ?>
 
-    <?= $form->field($model, 'event_startdate')->textInput() ?>
+    <?= $form->field($model, 'event_startdate')->widget(
+        DatePicker::className(), [
+            // inline too, not bad
+            'inline' => false, 
+            // modify template for custom rendering
+            //'template' => '<div class="well well-sm" style="background-color: #fff; width:250px">{input}</div>',
+            'clientOptions' => [
+                'autoclose' => true,
+                'format' => 'yyyy-mm-dd'
+            ]
+    ]);?>
 
-    <?= $form->field($model, 'event_enddate')->textInput() ?>
+    <?= $form->field($model, 'event_enddate')->widget(
+        DatePicker::className(), [
+            // inline too, not bad
+            'inline' => false, 
+            // modify template for custom rendering
+            //'template' => '<div class="well well-sm" style="background-color: #fff; width:250px">{input}</div>',
+            'clientOptions' => [
+                'autoclose' => true,
+                'format' => 'yyyy-mm-dd'
+            ]
+    ]);?>
 
-    <?= $form->field($model, 'event_starttime')->textInput() ?>
+    <?= $form->field($model, 'event_starttime')->widget(
+        TimePicker::className(), [
+            'name' => 'time',
+            'pluginOptions' => [
+                'showSeconds' => false,
+                'showMeridian' => false,
+                'minuteStep' => 1,
+                'secondStep' => 5
+            ]
+    ]);?>
 
-    <?= $form->field($model, 'event_endtime')->textInput() ?>
+    <?= $form->field($model, 'event_endtime')->widget(
+        TimePicker::className(), [
+            'name' => 'time',
+            'pluginOptions' => [
+                'showSeconds' => false,
+                'showMeridian' => false,
+                'minuteStep' => 1,
+                'secondStep' => 5
+            ]
+    ]);?>
 
     <?= $form->field($model, 'event_status')->textInput(['maxlength' => 20]) ?>
 

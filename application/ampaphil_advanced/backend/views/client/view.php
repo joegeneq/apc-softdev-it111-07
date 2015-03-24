8d915f5b07cb240bcae6fb96bbb7290a2904efed
Alyssa Jane Lino <alihjane.lino@gmail.com>
2015-03-24 23:49:10 +0800

    MODIFIED client/view.php

--- application/ampaphil_advanced/backend/views/client/view.php
+++ application/ampaphil_advanced/backend/views/client/view.php
@@ -6,7 +6,7 @@ use yii\widgets\DetailView;
 /* @var $this yii\web\View */
 /* @var $model app\models\Client */
 
-$this->title = $model->id;
+$this->title = $model->client_company;
 $this->params['breadcrumbs'][] = ['label' => 'Clients', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>

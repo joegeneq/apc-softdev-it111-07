3c42d58be76645cc89ee2cd423b854647edd1c35
Alyssa Jane Lino <alihjane.lino@gmail.com>
2015-03-24 23:49:37 +0800

    MODIFIED employee/update.php

--- application/ampaphil_advanced/backend/views/employee/update.php
+++ application/ampaphil_advanced/backend/views/employee/update.php
@@ -5,9 +5,9 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model app\models\Employee */
 
-$this->title = 'Update Employee: ' . ' ' . $model->id;
+$this->title = 'Update Employee: ' . ' ' . $model->emp_fname.' '.$model->emp_lname;
 $this->params['breadcrumbs'][] = ['label' => 'Employees', 'url' => ['index']];
-$this->params['breadcrumbs'][] = ['label' => $model->id, 'url' => ['view', 'id' => $model->id]];
+$this->params['breadcrumbs'][] = ['label' => $model->emp_fname.' '.$model->emp_lname, 'url' => ['view', 'id' => $model->id]];
 $this->params['breadcrumbs'][] = 'Update';
 ?>
 <div class="employee-update">

4521738521e60e8afe54efdc6fb353841c006a56
Alyssa Jane Lino <alihjane.lino@gmail.com>
2015-03-24 23:48:31 +0800

    MODIFIED applicant/view.php

--- application/ampaphil_advanced/backend/views/applicant/view.php
+++ application/ampaphil_advanced/backend/views/applicant/view.php
@@ -6,7 +6,7 @@ use yii\widgets\DetailView;
 /* @var $this yii\web\View */
 /* @var $model app\models\Applicant */
 
-$this->title = $model->id;
+$this->title = $model->app_fname.' '.$model->app_lname;
 $this->params['breadcrumbs'][] = ['label' => 'Applicants', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>

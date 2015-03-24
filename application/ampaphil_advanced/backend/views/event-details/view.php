8c8489edcb5789739056d9a25b23e952923bc77e
Alyssa Jane Lino <alihjane.lino@gmail.com>
2015-03-24 23:50:43 +0800

    MODIFIED event-details/view.php

--- application/ampaphil_advanced/backend/views/event-details/view.php
+++ application/ampaphil_advanced/backend/views/event-details/view.php
@@ -6,7 +6,7 @@ use yii\widgets\DetailView;
 /* @var $this yii\web\View */
 /* @var $model app\models\EventDetails */
 
-$this->title = $model->id;
+$this->title = $model->event_name;
 $this->params['breadcrumbs'][] = ['label' => 'Event Details', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>

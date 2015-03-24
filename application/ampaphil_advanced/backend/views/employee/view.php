1cd79fc004dd93b1b90d969fca8841e659d1bb11
Alyssa Jane Lino <alihjane.lino@gmail.com>
2015-03-24 23:50:02 +0800

    MODIFIED employee/view.php

--- application/ampaphil_advanced/backend/views/employee/view.php
+++ application/ampaphil_advanced/backend/views/employee/view.php
@@ -6,7 +6,7 @@ use yii\widgets\DetailView;
 /* @var $this yii\web\View */
 /* @var $model app\models\Employee */
 
-$this->title = $model->id;
+$this->title = $model->emp_fname.' '.$model->emp_lname;
 $this->params['breadcrumbs'][] = ['label' => 'Employees', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>

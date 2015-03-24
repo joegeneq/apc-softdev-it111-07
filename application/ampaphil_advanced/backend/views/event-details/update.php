4bde5dc2b42f4576c5141c3ebef4ff31464b9e0c
Alyssa Jane Lino <alihjane.lino@gmail.com>
2015-03-24 23:50:31 +0800

    MODIFIED event-details/update.php

--- application/ampaphil_advanced/backend/views/event-details/update.php
+++ application/ampaphil_advanced/backend/views/event-details/update.php
@@ -5,9 +5,9 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model app\models\EventDetails */
 
-$this->title = 'Update Event Details: ' . ' ' . $model->id;
+$this->title = 'Update Event Details: ' . ' ' . $model->event_name;
 $this->params['breadcrumbs'][] = ['label' => 'Event Details', 'url' => ['index']];
-$this->params['breadcrumbs'][] = ['label' => $model->id, 'url' => ['view', 'id' => $model->id]];
+$this->params['breadcrumbs'][] = ['label' => $model->event_name, 'url' => ['view', 'id' => $model->id]];
 $this->params['breadcrumbs'][] = 'Update';
 ?>
 <div class="event-details-update">

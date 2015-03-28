57afb43c7a80753ba3c83e74e8aa8b23d8e1b913
Danica Parian <danica.parian@gmail.com>
2015-03-28 10:24:55 +0800

    modified applicant/view.php

--- application/ampaphil_advanced/backend/views/applicant/view.php
+++ application/ampaphil_advanced/backend/views/applicant/view.php
@@ -23,6 +23,7 @@ $this->params['breadcrumbs'][] = $this->title;
                 'method' => 'post',
             ],
         ]) ?>
+        <?= Html::a('Create Talent Line', ['talent-line/create'], ['class' => 'btn btn-success']) ?>
     </p>
 
     <?= DetailView::widget([

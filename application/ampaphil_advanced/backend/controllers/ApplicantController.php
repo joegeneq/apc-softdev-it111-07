e77ff132dcc686cb82a4203bf1058b44026d6626
Alyssa Jane Lino <alihjane.lino@gmail.com>
2015-03-29 23:56:32 +0800

    modified ApplicantController.php

--- application/ampaphil_advanced/backend/controllers/ApplicantController.php
+++ application/ampaphil_advanced/backend/controllers/ApplicantController.php
@@ -3,6 +3,7 @@
 namespace backend\controllers;
 
 use Yii;
+use yii\filters\AccessControl;
 use app\models\Applicant;
 use app\models\ApplicantSearch;
 use yii\web\Controller;
@@ -17,6 +18,20 @@ class ApplicantController extends Controller
     public function behaviors()
     {
         return [
+        'access' => [
+                'class' => AccessControl::className(),
+                'rules' => [
+                    [
+                        'actions' => ['login', 'error'],
+                        'allow' => true,
+                    ],
+                    [
+                        'actions' => ['index','_form','create','_search','update','view'],
+                        'allow' => true,
+                        'roles' => ['@'],
+                    ],
+                ],
+            ],
             'verbs' => [
                 'class' => VerbFilter::className(),
                 'actions' => [

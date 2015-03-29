7049e0532b8ec736473d5f3df5f3e85cb6e97f69
Alyssa Jane Lino <alihjane.lino@gmail.com>
2015-03-30 00:03:17 +0800

    modified TalentLineController.php

--- application/ampaphil_advanced/backend/controllers/TalentLineController.php
+++ application/ampaphil_advanced/backend/controllers/TalentLineController.php
@@ -3,6 +3,7 @@
 namespace backend\controllers;
 
 use Yii;
+use yii\filters\AccessControl;
 use app\models\TalentLine;
 use app\models\TalentLineSearch;
 use yii\web\Controller;
@@ -17,6 +18,20 @@ class TalentLineController extends Controller
     public function behaviors()
     {
         return [
+        'access' => [
+                'class' => AccessControl::className(),
+                'rules' => [
+                    [
+                        'actions' => ['login', 'error'],
+                        'allow' => true,
+                    ],
+                    [
+                        'actions' => ['index','_form','create','_search','update','view'],
+                        'allow' => true,
+                        'roles' => ['@'],
+                    ],
+                ],
+            ],
             'verbs' => [
                 'class' => VerbFilter::className(),
                 'actions' => [

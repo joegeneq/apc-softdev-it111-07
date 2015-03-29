cc39389a30c4d16bcebdadce6bed90d2f891f4fa
Alyssa Jane Lino <alihjane.lino@gmail.com>
2015-03-30 00:00:45 +0800

    modified calendar.php

--- application/ampaphil_advanced/backend/views/site/calendar.php
+++ application/ampaphil_advanced/backend/views/site/calendar.php
@@ -6,16 +6,5 @@ $this->title = 'Calendar';
 ?>
 <div class="site-index">
 
-    <div class="body-content">
-        <?= \talma\widgets\FullCalendar::widget([
-		    'googleCalendar' => true,  // If the plugin displays a Google Calendar. Default false
-		    'loading' => 'Carregando...', // Text for loading alert. Default 'Loading...'
-		    'config' => [
-		        // put your options and callbacks here
-		        // see http://arshaw.com/fullcalendar/docs/
-		        'lang' => 'en-ca', // optional, if empty get app language
-		        
-		    ],
-		]); ?>
-    </div>
+    <object data='../../calendar/events/calendar.php' style='width:100%;min-height: 965px;'>
 </div>

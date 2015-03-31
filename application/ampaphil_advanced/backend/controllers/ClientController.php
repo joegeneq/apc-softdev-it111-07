bad34d4cfa702f35001390b77b89bca5fb496507
Alyssa Jane Lino <alihjane.lino@gmail.com>
2015-03-31 15:17:11 +0800

    modified ClientController.php

--- application/ampaphil_advanced/backend/controllers/ClientController.php
+++ application/ampaphil_advanced/backend/controllers/ClientController.php
@@ -18,20 +18,6 @@ class ClientController extends Controller
     public function behaviors()
     {
         return [
-        'access' => [
-                'class' => AccessControl::className(),
-                'rules' => [
-                    [
-                        'actions' => ['login', 'error'],
-                        'allow' => true,
-                    ],
-                    [
-                        'actions' => ['index','_form','create','_search','update','view'],
-                        'allow' => true,
-                        'roles' => ['@'],
-                    ],
-                ],
-            ],
             'verbs' => [
                 'class' => VerbFilter::className(),
                 'actions' => [

65ad630694dc16892a42ade74e41582e120cfcaa
Danica Faith Parian <danica.parian@gmail.com>
2015-04-09 18:11:50 +0800

    modified screening-sched/_form.php

--- application/ampaphil_advanced/backend/views/screening-sched/_form.php
+++ application/ampaphil_advanced/backend/views/screening-sched/_form.php
@@ -38,19 +38,6 @@ use kartik\select2\Select2;
             ]
     ]);?>
 
-    <?php 
-
-        if (($model->app_status) == 'Screening')
-        {
-            echo $form->field($model, 'app_status')->dropDownList(['' => 'Select Status',
-                                                          'Passed' => 'Passed', 
-                                                          'Failed' => 'Failed']
-                                                        );
-        }
-    ?>
-
-  
-
     <?= $form->field($model, 'employee_id')->widget(Select2::classname(), [
         'data' => ArrayHelper::map(Employee::find()->all(), 'id', 'emp_lname'),
         'language' => 'en',
